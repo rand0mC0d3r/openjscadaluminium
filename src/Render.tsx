6a8f794c6ccd53e123e9e7d9ed7cc0833ab907cf
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2024-12-03 15:04:44 +0100

    moved to babylon

--- src/Render.tsx
+++ src/Render.tsx
@@ -1,44 +1,88 @@
-import { stlSerializer } from '@jscad/io';
-import { transforms } from '@jscad/modeling';
-import { union } from '@jscad/modeling/src/operations/booleans';
-import { roundedCuboid } from '@jscad/modeling/src/primitives';
-import { setupRenderer } from '@jscad/web';
-import { useEffect, useRef } from 'react';
+import { Color3, Vector3 } from '@babylonjs/core'
+import { useRef, useState } from 'react'
+import {
+  Engine,
+  Scene,
+  useBeforeRender,
+  useClick,
+  useHover,
+} from 'react-babylonjs'
 
-const Render = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+const DefaultScale = new Vector3(1, 1, 1)
+const BiggerScale = new Vector3(1.25, 1.25, 1.25)
 
-  useEffect(() => {
-    if (canvasRef.current) {
-      const context = canvasRef.current.getContext('2d');
-      if (!context) return;
+const SpinningBox = (props) => {
+  // access Babylon scene objects with same React hook as regular DOM elements
+  const boxRef = useRef(null)
 
-      const create3DGeometry = () => {
-        const cuboid = roundedCuboid({
-          size: [10, 20, 5],
-          segments: 16,
-        });
-        // const shiftedCuboid = transforms.translate([15, 0, 0], cuboid);
-        // return union(cuboid, shiftedCuboid);
-        return cuboid;
-      };
+  const [clicked, setClicked] = useState(false)
+  useClick(() => setClicked((clicked) => !clicked), boxRef)
 
-      const geometry = create3DGeometry();
-      console.log(geometry)
-      const serializedData = stlSerializer.serialize({ binary: false }, geometry);
+  const [hovered, setHovered] = useState(false)
+  useHover(
+    () => setHovered(true),
+    () => setHovered(false),
+    boxRef
+  )
 
-      // Display or process serializedData as needed
-      console.log(serializedData); // Logs the serialized STL data
-      // setupRenderer({ viewerElement: '#viewer' }, [geometry]);
-
-      // Rendering can be integrated here using @jscad/web or a custom renderer
+  // This will rotate the box on every Babylon frame.
+  const rpm = 5
+  useBeforeRender((scene) => {
+    if (boxRef.current) {
+      // Delta time smoothes the animation.
+      const deltaTimeInMillis = scene.getEngine().getDeltaTime()
+      boxRef.current.rotation.y +=
+        (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000)
     }
-  }, []);
+  })
 
-  return <>
-  <div id="viewer"></div>
-    <canvas ref={canvasRef} width="500" height="500" />
-  </>
-};
+  return (
+    <box
+      name={props.name}
+      ref={boxRef}
+      size={2}
+      position={props.position}
+      scaling={clicked ? BiggerScale : DefaultScale}
+    >
+      <standardMaterial
+        name={`${props.name}-mat`}
+        diffuseColor={hovered ? props.hoveredColor : props.color}
+        specularColor={Color3.Black()}
+      />
+    </box>
+  )
+}
 
-export default Render;
+export default function SceneWithSpinningBoxes() {
+
+  return <div>
+    <Engine antialias adaptToDeviceRatio canvasId="babylonJS">
+      <Scene>
+        <arcRotateCamera
+          name="camera1"
+          target={Vector3.Zero()}
+          alpha={Math.PI / 2}
+          beta={Math.PI / 4}
+          radius={8}
+        />
+        <hemisphericLight
+          name="light1"
+          intensity={0.7}
+          direction={Vector3.Up()}
+        />
+        <SpinningBox
+          name="left"
+          position={new Vector3(-2, 0, 0)}
+          color={Color3.FromHexString('#EEB5EB')}
+          hoveredColor={Color3.FromHexString('#C26DBC')}
+        />
+        <SpinningBox
+          name="right"
+          position={new Vector3(2, 0, 0)}
+          color={Color3.FromHexString('#C8F4F9')}
+          hoveredColor={Color3.FromHexString('#3CACAE')}
+        />
+      </Scene>
+    </Engine>
+  </div>
+}
